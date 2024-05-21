9a84a24e94d82e43beed1ee40a5f8981be014924
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-05-21 23:16:36 +0200

    init app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+import ContactList from "./components/ContactList/ContactList";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </>
+  );
+}
+
+export default App;
